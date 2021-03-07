7f3a62724b56b4cacc4036dbfe58151669f46418
Anderson <anderson@prodwin.com.br>
2021-03-07 14:34:39 -0300

    Implementando componentes básicos
    Implementando componentes básicos

---
+++ BlazorComponentDemo/BlazorComponentDemo/BaseComponents/DateTimeBaseComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace BlazorComponentDemo.BaseComponents
+{
+    public class DateTimeBaseComponent : ComponentBase
+    {
+        public DateTime dateTime { get; set; }
+    }
+}
