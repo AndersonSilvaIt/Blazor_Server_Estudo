98364023f85fb01327da76d18d6614871ada0433
Anderson <anderson@prodwin.com.br>
2021-03-10 16:58:10 -0300

    Implementado exemplo de DI no Blazor
    Implementado exemplo de DI no Blazor

---
+++ BlazorService_DependencyInjection/BlazorService_DependencyInjection/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorService_DependencyInjection
+{
+    public interface ICustomerService
+    {
+        string Uid { get; set; }
+        Customer GetCustomerById(int id);
+    }
+}
--- BlazorService_DependencyInjection/BlazorService_DependencyInjection/Startup.cs
+++ BlazorService_DependencyInjection/BlazorService_DependencyInjection/Startup.cs
@@ -29,6 +29,7 @@ namespace BlazorService_DependencyInjection
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddScoped<ICustomerService, CustomerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
