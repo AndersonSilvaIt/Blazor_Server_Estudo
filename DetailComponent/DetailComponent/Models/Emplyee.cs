b1f993258207b831d0a9023cfc98ee1fa169ca14
Anderson <anderson@prodwin.com.br>
2021-03-09 22:31:06 -0300

    Component Detail Async
    Component Detail Async

---
+++ DetailComponent/DetailComponent/Models/Emplyee.cs
@@ -0,0 +1,15 @@
+﻿namespace DetailComponent.Models
+{
+    public class Emplyee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Department { get; set; }
+
+        public void LoadRecords()
+        {
+            // load some records for the employee
+        }
+    }
+}
